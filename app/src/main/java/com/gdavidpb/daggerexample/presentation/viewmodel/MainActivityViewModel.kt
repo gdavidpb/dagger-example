507a2682985260474b31f8b71666efdbf0b4efb0
gdavidpb <gerardoprado93@gmail.com>
2018-12-09 13:05:38 -0300

    use case + viewmodel

--- app/src/main/java/com/gdavidpb/daggerexample/presentation/viewmodel/MainActivityViewModel.kt
+++ app/src/main/java/com/gdavidpb/daggerexample/presentation/viewmodel/MainActivityViewModel.kt
@@ -1,8 +1,17 @@
 package com.gdavidpb.daggerexample.presentation.viewmodel
 
 import android.arch.lifecycle.ViewModel
+import com.gdavidpb.daggerexample.domain.model.Post
+import com.gdavidpb.daggerexample.domain.model.WrappedLiveData
+import com.gdavidpb.daggerexample.domain.usecase.service.GetPostsUseCase
 import javax.inject.Inject
 
-open class MainActivityViewModel @Inject constructor() : ViewModel() {
+open class MainActivityViewModel @Inject constructor(
+    private val getPostsUseCase: GetPostsUseCase
+) : ViewModel() {
+    val posts = WrappedLiveData<List<Post>>()
 
+    fun getPosts() {
+        getPostsUseCase.execute(liveData = posts)
+    }
 }
